c84abd03b2e7037bd9330109a5541098b2a06df2
projector-rider-5 <yascr22@example.com>
2022-11-30 11:06:55 +0000

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:csharp/Tennis/TennisGame3.cs

--- csharp/Tennis/TennisGame3.cs
+++ csharp/Tennis/TennisGame3.cs
@@ -6,6 +6,7 @@ namespace Tennis
         private int p1; 
         private string p1N;
         private string p2N;
+        private readonly string[] _pointsToString = { "Love", "Fifteen", "Thirty", "Forty" };
 
         public TennisGame3(string player1Name, string player2Name)
         {
@@ -13,27 +14,33 @@ namespace Tennis
             this.p2N = player2Name;
         }
 
-        public bool isAdvantage()
+        public bool IsNormalScoring()
         {
-            return !((p1 < 4 && p2 < 4) && (p1 + p2 < 6));
+            return ((p1 < 4 && p2 < 4) && (p1 + p2 < 6));
         }
+        
         public string GetScore()
         {
-            string s;
-            if (!isAdvantage())
+            if (IsNormalScoring())
             {
-                string[] p = { "Love", "Fifteen", "Thirty", "Forty" };
-                s = p[p1];
-                return (p1 == p2) ? s + "-All" : s + "-" + p[p2];
+                return (p1 == p2) ? _pointsToString[p1] + "-All" : _pointsToString[p1] + "-" + _pointsToString[p2];
             }
             else
             {
-                if (p1 == p2)
+                if (IsDeuce())
                     return "Deuce";
-                s = p1 > p2 ? p1N : p2N;
-                return ((p1 - p2) * (p1 - p2) == 1) ? "Advantage " + s : "Win for " + s;
+                var s = p1 > p2 ? p1N : p2N;
+                
+                if (IsAdvantage())
+                    return "Advantage " + s;
+                else
+                    return "Win for " + s;
             }
         }
+
+        private bool IsDeuce() => p1 == p2;
+
+        private bool IsAdvantage() => (p1 - p2) * (p1 - p2) == 1;
 
         public void WonPoint(string playerName)
         {
